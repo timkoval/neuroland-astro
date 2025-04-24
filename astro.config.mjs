1a9fcf2ffb0006dc8c5005dcbed4b0b87ba3cd3f
Tim Koval <timkoval00@gmail.com>
2025-04-24 17:26:33 +0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+// import react from "@astrojs/react";
+import icon from "astro-icon";
+// import partytown from "@astrojs/partytown";
+import { remarkWikiLink } from "./src/plugins/remark-wiki-link";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://maggieappleton.com",
+  image: {
+    domains: ["res.cloudinary.com"],
+  },
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkWikiLink],
+      shikiConfig: {
+        theme: "night-owl",
+        wrap: true,
+      },
+    }),
+    // partytown({
+    //   config: {
+    //     forward: ["dataLayer.push"],
+    //   },
+    // }),
+    // react(),
+    icon(),
+  ],
+});
